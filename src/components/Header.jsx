6c3016023207f519f26f35fc144a7a5d8e22cad9
FellipeCastro <fellipecastro27d@gmail.com>
2023-12-24 12:06:08 -0300

    Header responsivo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,43 @@
+import { useState } from "react";
+
+import { IoMdMenu } from "react-icons/io";
+
 import './Header.css'
 
 export default function Header() {
+
+    const [isOpen, setIsopen] = useState(false)
+
+    const openMenu = () => {
+        setIsopen(true)
+        document.querySelector("#navbar").style.display = "flex"
+    }
+
+    const closeMenu = () => {
+        setIsopen(false)
+        document.querySelector("#navbar").style.display = "none"
+    }
+
+    const handleLinkClick = () => {
+        if (isOpen) {
+            closeMenu();
+        }
+    }
+
     return (
         <header className='header'>
             <h1>Fellipe Castro</h1>
 
-            <nav>
-                <a href="#home">Home</a>
-                <a href="#about">Sobre mim</a>
-                <a href="#projects">Projetos</a>
-                <a href="#contact">Contato</a>
+            <nav className="navbar" id="navbar">
+                <a href="#home" onClick={handleLinkClick}>Home</a>
+                <a href="#about" onClick={handleLinkClick}>Sobre mim</a>
+                <a href="#projects" onClick={handleLinkClick}>Projetos</a>
+                <a href="#contact" onClick={handleLinkClick}>Contato</a>
             </nav>
+
+            <button className="menu" onClick={isOpen ? closeMenu : openMenu}>
+                <IoMdMenu />
+            </button>
         </header>
     )
 }
