b76a5b3414a9440a9ff3e32a7ea74b6421d99de1
FellipeCastro <fellipecastro27d@gmail.com>
2024-01-03 18:08:58 -0300

    Modais funcionando

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react'
+
 import html5 from '../img/html-5.jpg'
 import css3 from '../img/css-3.jpg'
 import javascript from '../img/javascript.jpg'
 import reactjs from '../img/reactjs.jpg'
 import git from '../img/git.jpg'
 
-import './About.css'
 import ModalSkills from './ModalSkills'
+
+import './About.css'
 
 export default function About() {
 
+    const [skillData, setSkillData] = useState({
+        img: "",
+        title: "",
+        description: ""
+    })
+
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    
     const skills = [
         {name: "HTML 5", src: html5, description: "HTML5 é a quinta versão da linguagem de marcação utilizada para estruturar conteúdo na web. Introduziu novos elementos e APIs que aprimoram a semântica, o suporte multimídia e a interatividade nas páginas da web."},
         {name: "CSS 3", src: css3, description: "CSS3 é a terceira versão das Folhas de Estilo em Cascata e é usada para estilizar o layout e a apresentação de páginas web. Introduziu recursos como transições, animações, sombras e bordas arredondadas, oferecendo maior controle estilístico."},
@@ -16,6 +27,20 @@ export default function About() {
         {name: "React JS", src: reactjs, description: " ReactJS é uma biblioteca de JavaScript desenvolvida pelo Facebook. É usada para construir interfaces de usuário reativas e eficientes, facilitando a criação de componentes reutilizáveis e o desenvolvimento de aplicações de página única (SPA)."},
         {name: "Git e Github", src: git, description: "Git é um sistema de controle de versão distribuído amplamente utilizado para rastrear alterações no código-fonte durante o desenvolvimento de software. GitHub é uma plataforma de hospedagem de código-fonte que utiliza o Git. É popular para colaboração em projetos de software, fornecendo recursos como controle de versão, colaboração, rastreamento de problemas e integração contínua."}
     ]
+
+    const openModal = (skill) => {
+        setSkillData({
+            img: skill.src,
+            title: skill.name,
+            description: skill.description
+        })
+
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
 
     return (
         <section className='about' id='about'>
@@ -36,7 +61,7 @@ export default function About() {
                     <ul>
                         {skills.map((skill, index) => {
                             return (
-                                <li key={index}>
+                                <li key={index} onClick={() => openModal(skill)}>
                                     <img src={skill.src} alt={skill.name} />
                                     <h3>{skill.name}</h3>
                                 </li>
@@ -45,6 +70,15 @@ export default function About() {
                     </ul>
                 </div>
             </div>
+
+            {isModalOpen && (
+                <ModalSkills 
+                    img={skillData.img}
+                    title={skillData.title}
+                    description={skillData.description}
+                    closeModal={closeModal}
+                />
+            )}
         </section>
     )
 }
--- src/components/ModalSkills.jsx
+++ src/components/ModalSkills.jsx
@@ -1,13 +1,12 @@
 import { IoMdClose } from "react-icons/io";
 
-
 import './ModalSkills.css'
 
-export default function ModalSkills({ img, title, description }) {
+export default function ModalSkills({ img, title, description, closeModal }) {
     return (
         <div className='modal'>
-            <button className='close-btn'>
-            <IoMdClose />
+            <button className='close-btn' onClick={() => closeModal()}>
+                <IoMdClose />
             </button>
             <div className="img-container-modal">
                 <img src={img} alt={title} />
